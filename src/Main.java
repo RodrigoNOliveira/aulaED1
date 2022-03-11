fff6a86ff3bd833b2786704caa0022215f11bfe4
Matheus <kannon.marih@protonmail.com>
2022-03-10 21:21:34 -0300

    codigo da primeira aula

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Vetor vetor = new Vetor(5);
+       
+        System.out.println(vetor);
+        
+    }
+
+
+}
+         
