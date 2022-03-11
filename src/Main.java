f1a6cd99de33e9db39f60cb59d93984d5eb1f69f
Matheus <kannon.marih@protonmail.com>
2022-03-10 22:37:39 -0300

    Alteração

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Vetor vetor = new Vetor(5);
+    public static void main(String[] args) {
+        
+        try {
+       Vetor vetor = new Vetor(5);
+        vetor.addValor("Dois");
+        vetor.addValor("Tres");
+        vetor.addValor("um");
+      
        
-        System.out.println(vetor);
+        System.out.println(vetor);}
+        catch( Exception e) {
+        	e.printStackTrace();
+        }
         
     }
 
--- src/Vetor.java
+++ src/Vetor.java
@@ -2,13 +2,51 @@ import java.util.Arrays;
 
 public class Vetor {
     private String[] elementos;
+    private int tamanho;
 
     public Vetor(int capacidade){
         elementos = new String[capacidade];
+        tamanho = 0;
     }
 
+	
+	
+	/*public void addValor(String elemento) {
+		for (int i = 0; i< elementos.length; i++) {
+			if (elementos[i] == null) {
+				elementos[i] = elemento;
+				tamanho++;
+				break;
+			}
+		}
+	}*/
+    
+    public void addValor(String elemento) throws Exception {
+    	if ( tamanho < elementos.length) {
+    		elementos[tamanho] = elemento;
+    		tamanho++;
+    	}
+    	else {
+    		throw new Exception("Vetor cheio");
+    	}
+    }
+	
+	
 	@Override
 	public String toString() {
-		return "Vetor [elementos=" + Arrays.toString(elementos) + "]";
+		
+		StringBuffer s = new StringBuffer();
+		s.append("[");
+		for (int i = 0; i< tamanho-1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+		
+		if (tamanho >0 ) {
+			s.append(elementos[tamanho-1]);
+		}
+		
+		s.append("]");
+		return s.toString();
 	}
 }
